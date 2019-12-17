1fe411b4554f5e5bbe8a5da4ca7615e1b5aeb1fd
viktor <kosandyak1996@gmail.com>
2019-12-17 23:19:48 +0200

    Starting a conversation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
 
     @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages)
-                                 .find(session[:conversations])
+                                 .find(session[:conversations])                        
   end
 end
